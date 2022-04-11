90353e931b882d4a39b0ff849a88aa35da6108fc
xkittx <git@xkittx.com>
2022-04-11 15:42:05 +0900

    Fix site url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://example.com`,
   },
   plugins: [],
 };
